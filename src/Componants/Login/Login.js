d95de892fd0eb60c1d46b43314a354540f3a2957
kmbarnett <kmbarnett94@gmail.com>
2020-04-15 21:02:50 -0600

    Fix redirect instead of link so that store updates proporly

--- src/Componants/Login/Login.js
+++ src/Componants/Login/Login.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Login.css';
 import { logIn } from '../../actions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
   constructor(props) {
@@ -21,64 +21,48 @@ class Login extends Component {
     })
   }
 
-  submitForm = (event) => {
-    const { name, cohort } = this.state
-    event.preventDefault();
-    this.props.logIn({name, cohort })
+  submitForm = (e) => {
+    const {name, cohort, play} = this.state
+    e.preventDefault();
+    this.props.logIn({name, cohort})
     this.setState({play: true})
   }
 
   render() {
-    const { name, cohort } = this.state
+    const {name, cohort, play} = this.state
     const filledOut = (name && cohort)
-    return (
-      <form onSubmit={this.submitForm} className='log-in-form' >
-        <label htmlFor='name-input'>Name:
-          <input
-            id='name-input'
-            type='text'
-            name='name'
-            className='login-input'
-            value={this.state.name}
-            placeholder='Your Name'
-            onChange={this.handleChange}
-            required
-            />
-        </label>
-        <label htmlFor='cohort-input'>Cohort:
-          <select
-            id="cohort-input"
-            name='cohort'
-            className='login-input'
-            value={this.state.password}
-            placeholder='Your password'
-            onChange={this.handleChange}
-            required
-            defaultValue='pick cohort'
-            >
-            <option disabled hidden value="pick cohort">Pick Cohort</option>
-            <option value="1909">1909</option>
-            <option value="1911">1911</option>
-            <option value="2001">2001</option>
-            <option value="2003">2003</option>
-          </select>
-        </label>
-        <Link to='/game'>
-          <button disabled={!filledOut} type="submit" >
-            Let's Play
-          </button>
-        </Link>
-      </form>
-    )
+    const redirectPath = play ? '/game' : '/';
+    
+    return (<form onSubmit={this.submitForm} className='log-in-form'>
+      <Redirect to={redirectPath}/>
+      <label htmlFor='name-input'>Name:
+        <input id='name-input' type='text' name='name' className='login-input' value={this.state.name} placeholder='Your Name' onChange={this.handleChange} required="required"/>
+      </label>
+      <label htmlFor='cohort-input'>Cohort:
+        <select id="cohort-input" name='cohort' className='login-input' value={this.state.password} placeholder='Your password' onChange={this.handleChange} required="required" defaultValue='pick cohort'>
+          <option disabled="disabled" hidden="hidden" value="pick cohort">Pick Cohort</option>
+          <option value="1909">1909</option>
+          <option value="1911">1911</option>
+          <option value="2001">2001</option>
+          <option value="2003">2003</option>
+        </select>
+      </label>
+      <button disabled={!filledOut} type="submit">
+        Let's Play
+      </button>
+    </form>)
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  logIn: (user) => dispatch( logIn(user) ),
+  logIn: (user) => {
+    dispatch(logIn(user))
+    console.log('done')
+  }
 })
 
-export default Login;
+export default connect(null, mapDispatchToProps)(Login);
 
 Login.propTypes = {
-  logIn: PropTypes.func.isRequired,
+  logIn: PropTypes.func.isRequired
 }
