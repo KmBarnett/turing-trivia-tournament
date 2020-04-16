59939ee847c3328f307e28bd5cfa43bcb18a1063
kmbarnett <kmbarnett94@gmail.com>
2020-04-16 10:46:10 -0600

    Add geting all questions

--- src/Componants/App/App.js
+++ src/Componants/App/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Login from './../Login/Login.js'
 import Instructions from './../Instructions/Instructions.js'
+import GameTimer from './../Timer/Timer.js'
+import Question from './../../Containers/Question/Question.js'
+import { getQuestions } from '../../actions';
 
 
 class App extends Component {
+
+ async componentDidMount() {
+    try {
+      const response = await fetch('https://opentdb.com/api.php?amount=10')
+      const date = await response.json()
+      this.props.getQuestions(await date.results)
+      }
+    catch(error) {
+      console.error(error);
+    }
+  }
 
   render() {
     return (
@@ -16,9 +31,16 @@ class App extends Component {
         <Route exact path='/game' >
           <Instructions />
         </Route>
+        <Route exact path='/game/start' >
+          <Question />
+        </Route>
       </main>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  getQuestions: (questions) => dispatch(getQuestions(questions))
+})
+
+export default connect(null, mapDispatchToProps)(App);
