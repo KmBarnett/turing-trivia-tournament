09f4da8d6a734cdc6231acb8fde8a88a82d3f9ad
kmbarnett <kmbarnett94@gmail.com>
2020-04-16 19:49:05 -0600

    Add answers reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import { user } from './user';
 import { questions } from './questions';
+import { answers } from './answers';
 
-const rootReducer = combineReducers({user, questions})
+const rootReducer = combineReducers({user, questions, answers})
 
 export default rootReducer;
