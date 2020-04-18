dabf9ee8407628f8e93344d8f30b1decd3ea1f4c
kmbarnett <kmbarnett94@gmail.com>
2020-04-18 15:31:53 -0600

    Fix remove delay in timer spin

--- src/Componants/Timer/Timer.js
+++ src/Componants/Timer/Timer.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 import './Timer.css';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { logTime } from '../../actions';
 
 
 class GameTimer extends Component {
   constructor() {
     super()
     this.state = {
-      active: false,
       redirectPath: '/game/start'
     }
   }
@@ -17,8 +17,19 @@ class GameTimer extends Component {
     this.setState({redirectPath: '/game/end'})
   }
 
-  startTimer() {
-    this.setState({active: true})
+  trackTime(second) {
+    this.props.logTime(second);
+  }
+
+  createCheckpoints() {
+    const checkpoints = []
+    for (let i = 60000; i > 0; i -= 1000) {
+      checkpoints.push({
+        time: i,
+        callback: () => this.trackTime(i/1000 - 1),
+      })
+    }
+    return checkpoints
   }
 
   render() {
@@ -26,7 +37,7 @@ class GameTimer extends Component {
     const { active, redirectPath } = this.state
     return (
       <div className='Timer'>
-        <div className={'animated-timer' + (active && ' timer-started')}></div>
+        <div className='animated-timer timer-started'></div>
         <Redirect to={redirectPath}/>
         <div className='timer-display'>
           <Timer
@@ -38,10 +49,7 @@ class GameTimer extends Component {
                 time: 0,
                 callback: () => this.endGame(),
               },
-              {
-                time: startTime - 1,
-                callback: () => this.startTimer(),
-              },
+              ...this.createCheckpoints()
             ]}
             >
             {() => (
@@ -57,4 +65,8 @@ class GameTimer extends Component {
   }
 }
 
-export default connect(null, null)(GameTimer);
+const mapDispatchToProps = dispatch => ({
+  logTime: (time) => dispatch(logTime(time))
+})
+
+export default connect(null, mapDispatchToProps)(GameTimer);
