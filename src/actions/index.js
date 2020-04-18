582d00024e1c05dfb8d24f4aee482147e8fec43e
kmbarnett <kmbarnett94@gmail.com>
2020-04-18 15:06:13 -0600

    Add score and time tracking

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,13 @@ export const answerQuestion = (answer) => ({
   type: 'ANSWER_QUESTION',
   answer
 })
+
+export const logScore = (scoreData) => ({
+  type: 'LOG_SCORE',
+  scoreData
+})
+
+export const logTime = (time) => ({
+  type: 'LOG_TIME',
+  time
+})
