1aead2a2a8b5d8e42eed138ec225a90b5b0acd05
kmbarnett <kmbarnett94@gmail.com>
2020-04-18 15:05:02 -0600

    Add setup for winner

---
+++ src/Containers/Winner/Winner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Winner.css';
+
+
+function Winner() {
+
+
+    return (
+      <section className='Winner'>
+        
+      </section>
+    );
+}
+
+
+export default Winner;
