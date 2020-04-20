e627a2e95397267825fa65b9fd5abc8333df5f92
kmbarnett <kmbarnett94@gmail.com>
2020-04-20 14:41:43 -0600

    Add setCatagory and getCatagories

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,20 @@ export const logIn = user => ({
   user
 })
 
+export const logOut = () => ({
+  type: 'LOG_OUT',
+})
+
+export const getCategories = categories => ({
+  type: 'GET_CATEGORIES',
+  categories
+})
+
+export const setCategory = (category) => ({
+  type: 'SET_CATEGORY',
+  category
+})
+
 export const getQuestions = questions => ({
   type: 'GET_QUESTIONS',
   questions
@@ -11,6 +25,10 @@ export const getQuestions = questions => ({
 export const answerQuestion = (answer) => ({
   type: 'ANSWER_QUESTION',
   answer
+})
+
+export const clearAnswers = () => ({
+  type: 'CLEAR_ANSWERS',
 })
 
 export const logScore = (scoreData) => ({
