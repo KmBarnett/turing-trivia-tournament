97b3ff4457f2bb685cc8a5815927fb582433e1c1
kmbarnett <kmbarnett94@gmail.com>
2020-04-20 14:50:45 -0600

    Add new game button and some display needs

--- src/Containers/Leaderboard/Leaderboard.js
+++ src/Containers/Leaderboard/Leaderboard.js
@@ -1,41 +1,85 @@
 import React from 'react';
 import './Leaderboard.css';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logOut, clearAnswers } from '../../actions';
+import converter from 'number-to-words';
 
 
-function Leaderboard({ data, display }) {
+function Leaderboard(props) {
   let entries;
 
-  function createSpot({name, score, cohort, time, index}) {
+  function createSpot({name, score, cohort, time, index, category}) {
     let program;
     if (cohort) {
       program = cohort.includes('BE') ? 'BE' : 'FE';
     }
-    return <li key={index} className={program}> {name}: {Math.trunc(score)} Time: {time}</li>
+    if (!name) {
+    return  <li key={index}> {converter.toOrdinal(index + 1)}: </li>
+    }
+    return <li
+      key={index}
+      className={`${program} placed`}>
+        {converter.toOrdinal(index + 1)}:
+      <section className='table-rows'>
+        <p>
+          {name}
+        </p>
+        <p>
+          {score}
+        </p>
+        {time}Sec
+        <p>
+          {category}
+        </p>
+        <p>
+          {cohort}
+        </p>
+      </section>
+    </li>
   }
 
-  if (display !== 'cohorts') {
-    const anon = {cohort: '', score: 1000, name:'Alan Turing', time: 60}
+  function runDispatch() {
+    props.logOut()
+    props.clearAnswers()
+  }
+
     const leadScafold = new Array(10);
-    leadScafold.fill({...anon}, 0, 10)
-    const sortedData = [...data, ...leadScafold].sort((a,b) => b.score - a.score)
+    const sortedData = [...props.scores, ...leadScafold].sort((a,b) => b.score - a.score)
     entries = sortedData.slice(0, 10).map((entry, index) => {
       return createSpot({...entry, index})})
-  } else {
-    const sortedData = [...data].sort((a,b) => b.score - a.score)
-    entries = sortedData.slice(0, 10).map((entry, index) => {
-      return createSpot({...entry, index})})
-  }
-
 
 
     return (
-      <section>
+      <section className='Leaderboard'>
         <ol>
+          <li>
+            <section className='table-heads'>
+              <h3>Name</h3>
+              <h3>Score</h3>
+              <h3>Time</h3>
+              <h3>Category</h3>
+              <h3>Cohort</h3>
+            </section>
+          </li>
           {entries}
         </ol>
+        <Link to='/'>
+          <button onClick={runDispatch} type='button'>New Game</button>
+        </Link>
       </section>
     );
 }
 
 
-export default Leaderboard;
+const mapStateToProps = state => ({
+  scores: state.scores,
+})
+
+const mapDispatchToProps = dispatch => ({
+  logOut: () => { dispatch( logOut() ) },
+  clearAnswers: () => { dispatch( clearAnswers() ) }
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Leaderboard);
