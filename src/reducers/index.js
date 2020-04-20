b5203aa21757a4d51ea57b5eaadc28d82066cf6a
kmbarnett <kmbarnett94@gmail.com>
2020-04-20 14:40:07 -0600

    Add catagories and category

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,17 @@ import { questions } from './questions';
 import { answers } from './answers';
 import { scores } from './scores';
 import { time } from './time';
+import { categories } from './categories';
+import { category } from './category';
 
-const rootReducer = combineReducers({user, questions, answers, scores, time})
+const rootReducer = combineReducers({
+  user,
+  questions,
+  answers,
+  scores,
+  time,
+  category,
+  categories
+})
 
 export default rootReducer;
